e463825f32cac1812e6ab368c2e84b66a2c331d3
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-03 11:20:40 +0100

    created post route to add todos

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.use(express.json());
 //Routes
 
 //create  a todo
+app.post('/todos', async (req, res) => {
+  try {
+    const { description } = req.body;
+
+    const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *', [
+      description
+    ]);
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //get all todos
 
