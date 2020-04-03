0d1e236980448a6a5b177c55766317e0bd7144db
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-03 14:38:32 +0100

    created todo list component to display list of added todos

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/todos');
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <>
+      <table class='table mt-5 text-center'>
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map(todo => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td>
+                <button>Edit</button>
+              </td>
+              <td>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ListTodos;
