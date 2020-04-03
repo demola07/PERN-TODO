26f3463f3161f54cbcd68a3af3613df8f3a12b93
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-03 11:01:44 +0100

    created connection to postgres database

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const pool = require('./db');
 const PORT = 5000;
 
 ///midleware
 app.use(cors());
 app.use(express.json());
+
+//Routes
+
+//create  a todo
+
+//get all todos
+
+//get a todo
+
+//update a todo
+
+//delete a todo
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
