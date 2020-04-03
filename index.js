6030c8dfbdd75c87a1907fa77b986e500fe032fb
demola07 <45898468+demola07@users.noreply.github.com>
2020-04-03 12:38:48 +0100

    created get a single todo, update todo and delete todo routes

--- index.js
+++ index.js
@@ -34,10 +34,42 @@ app.get('/todos', async (req, res) => {
 });
 
 //get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE todo_id = $1', [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+
+    const updateTodo = await pool.query('UPDATE todo SET description = $1 WHERE todo_id = $2', [
+      description,
+      id
+    ]);
+    res.json('Todo was updated');
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await pool.query('DELETE FROM todo WHERE todo_id=$1', [id]);
+    res.json('Todo was deleted');
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
